6dfe6e1af0ede27e87880ec56f89c7acd0e78632
isgilbert <isabellasgilbert@gmail.com>
2024-09-15 14:07:13 -0400

    redis hit counter

--- app.py
+++ app.py
@@ -5,6 +5,19 @@ from flask import Flask
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
 
+
 @app.route('/')
 def webapp():
-    return "hi!"
+    return "here's a hit counter: " + str(get_hit_count())
+
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
